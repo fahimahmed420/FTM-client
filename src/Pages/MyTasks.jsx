5123b5346d8f811dbac89be08036fa8733e65023
Fahim Ahmed <asifahmed55445@gmail.com>
2025-05-24 13:00:15 +0600

    fix a little thing

--- src/Pages/MyTasks.jsx
+++ src/Pages/MyTasks.jsx
@@ -85,7 +85,7 @@ const MyTasks = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center min-h-[40vh]">
+      <div className="flex justify-center items-center min-h-[73vh] py-10">
         <span className="loading loading-bars loading-lg"></span>
       </div>
     );
